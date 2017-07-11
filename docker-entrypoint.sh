28b7d5cd8785aee43cf7745a7543b7dfabe20af8
Kévin Darcel <kevin.darcel@gmail.com>
2017-07-11 15:06:55 -0500

    Fetch tsdns settings before launching it

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,8 @@ home_dir=/home/teamspeak/teamspeak3-server_linux_amd64/tsdns
 
 cd ${home_dir}
 
+curl ${MEH_MAP_URL} > /home/teamspeak/teamspeak3-server_linux_amd64/tsdns/tsdns_settings.ini
+
 nohup ./updater.py &
 
 echo ">> Starting TSDNS server..."
