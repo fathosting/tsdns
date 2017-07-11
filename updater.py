35069d3fa7a3e668c49f080ee02bebf7446ab121
Kévin Darcel <kevin.darcel@gmail.com>
2017-07-11 16:37:06 -0500

    Handle GET request for the health test

--- updater.py
+++ updater.py
@@ -9,15 +9,20 @@ import subprocess
 settings_filepath = "/home/teamspeak/teamspeak3-server_linux_amd64/tsdns/tsdns_settings.ini"
 
 class MyHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+    self.send_response(200)
+    self.wfile.write(b'OK')
+
   def do_POST(self):
     urllib.request.urlretrieve(os.environ.get('MEH_MAP_URL'), settings_filepath)
     subprocess.call(["./tsdnsserver", "--update"])
 
-    self.send_response(200)
     self.send_header('Content-type', 'text/html')
     self.end_headers()
+    self.send_response(200)
     self.wfile.write(b'OK')
-    return
 
 if __name__ == '__main__':
   if not os.environ.get('MEH_MAP_URL'):
